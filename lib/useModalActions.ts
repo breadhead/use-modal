5ed1b601c99e3c9e42b0fcdd9da21951389be114
Igor Kamyshev <igor@kamyshev.me>
2019-05-09 15:18:51 +0300

    fix: fix import

--- lib/useModalActions.ts
+++ lib/useModalActions.ts
@@ -2,7 +2,7 @@ import { useCallback, useContext } from 'react'
 
 import { updateRoute } from './helpers/updateRoute'
 import { MODAL_QUERY_PREFIX } from './helpers/MODAL_QUERY_PREFIX'
-import { MODAL_DEFAULT_VALUE } from 'helpers/MODAL_DEFAULT_VALUE'
+import { MODAL_DEFAULT_VALUE } from './helpers/MODAL_DEFAULT_VALUE'
 import { ModalContext } from './ModalContext'
 
 export const useModalActions = (key: string) => {
